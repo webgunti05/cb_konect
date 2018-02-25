5a0167e19db67275e826b38f4eedf04beea2391e
Anji <guntianjaneyulu05@gmail.com>
2018-02-26 00:34:06 +0530

    server-setup

--- client/src/app/members/members.component.ts
+++ client/src/app/members/members.component.ts
@@ -7,8 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MembersComponent implements OnInit {
 
-  constructor() { }
+  private showActionsOne: boolean;
+  private showActionsTwo: boolean;
+  private showActionsThree: boolean;
+  constructor() {
+    this.showActionsOne = false;
+    this.showActionsTwo = false;
+    this.showActionsThree = false;
+  }
 
+  showPopActionsOne() {
+    this.showActionsOne = true;
+    this.showActionsTwo = false;
+    this.showActionsThree = false;
+  }
+  showPopActionsTwo() {
+    this.showActionsOne = false;
+    this.showActionsTwo = true;
+    this.showActionsThree = false;
+  }
+  showPopActionsThree() {
+    this.showActionsOne = false;
+    this.showActionsTwo = false;
+    this.showActionsThree = true;
+  }
+
+  hidePopActions() {
+    this.showActionsOne = false;
+    this.showActionsTwo = false;
+    this.showActionsThree = false;
+  }
   ngOnInit() {
   }
 
